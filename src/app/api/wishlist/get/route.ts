23b97bfc6cb10674c5bd5e7b9f29d58bc6544e9b
UrPo2006 <romaibrahim@gmail.com>
2025-12-25 19:59:12 +0200

    'apdated'

--- src/app/api/wishlist/get/route.ts
+++ src/app/api/wishlist/get/route.ts
@@ -2,14 +2,21 @@ import { getDecodedToken } from "@/app/api/helpers/auth";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const decoded = await getDecodedToken()
+  const decoded = await getDecodedToken();
+
+  
+  if (!decoded?.token) {
+    return new Response("Not authenticated", { status: 401 });
+  }
+
   const res = await fetch("https://ecommerce.routemisr.com/api/v1/wishlist", {
     headers: {
       "Content-Type": "application/json",
-       token: decoded?.token as string,
+      token: decoded.token, 
     },
   });
 
   const data = await res.json();
+
   return NextResponse.json(data);
 }
